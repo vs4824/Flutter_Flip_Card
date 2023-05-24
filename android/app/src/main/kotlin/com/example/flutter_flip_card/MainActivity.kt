e2bcb1a8cf0fa2d110e9a1027c02d6472b2f6193
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-24 14:01:09 +0530

    Flutter Flip Card

---
+++ android/app/src/main/kotlin/com/example/flutter_flip_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_flip_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+import 'package:flip_card/flip_card.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlipCard',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  _renderAppBar(context) {
+    return MediaQuery.removePadding(
+      context: context,
+      removeBottom: true,
+      child: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+
+  _renderContent(context) {
+    return Card(
+      elevation: 0.0,
+      margin: const EdgeInsets.fromLTRB(32.0, 20.0, 32.0, 0.0),
+      color: Colors.black,
+      child: FlipCard(
+        direction: FlipDirection.HORIZONTAL,
+        side: CardSide.FRONT,
+        speed: 1000,
+        onFlipDone: (status) {
+          // ignore: avoid_print
+          print(status);
+        },
+        front: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFF006666),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Front', style: Theme.of(context).textTheme.displayLarge),
+              Text('Click here to flip back',
+                  style: Theme.of(context).textTheme.bodyLarge),
+            ],
+          ),
+        ),
+        back: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF006666),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Back',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              Text(
+                'Click here to flip front',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlipCard'),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _renderAppBar(context),
+          Expanded(
+            flex: 4,
+            child: _renderContent(context),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
